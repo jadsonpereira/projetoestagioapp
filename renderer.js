37396a026586b76b48710be311072b5e87f8c6d5
jadsonpereira <kewdesigner@hotmail.com>
2017-03-08 14:41:29 -0300

    proj

---
+++ renderer.js
@@ -0,0 +1,34 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+var loki = require('lokijs');
+var db = new loki('db.json')
+var clientes = db.addCollection('clientes');
+//clientes.insert({
+//nome:'Jadson',
+//email:'jsantospereira007@gmail.com'
+//});
+//db.save()
+
+function ready(fn){
+	if (document.readyState != 'loading'){
+		fn();
+		
+	}else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+}
+ready(function){
+     document.querySelector('#salvar').addEventListener('click', function(e){
+		e.preventDefault();
+	let data = {
+		nome:document.querySelector('#nome').value,
+		cpf:document.querySelector('#cpf').value,
+		telefone:document.querySelector('#telefone').value
+	};
+	clientes.insert(data);
+    db.save();
+	alert('Salvo com sucesso');
+	document.querySelector('#cadastro-cliente').reset()
+	})	
+})
